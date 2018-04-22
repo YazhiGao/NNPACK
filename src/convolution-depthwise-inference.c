87fe3240c11d71ad7beafc5c9049cb9a308f9b10
Likun Lei <llei@andrew.cmu.edu>
2018-04-22 19:43:22 -0400

    add clang-format

--- src/convolution-depthwise-inference.c
+++ src/convolution-depthwise-inference.c
@@ -19,21 +19,19 @@
 
 enum nnp_status nnp_convolution_depthwise_inference(
     enum nnp_convolution_algorithm algorithm,
-    enum nnp_convolution_transform_strategy transform_strategy,
-    size_t input_channels, size_t output_channels, struct nnp_size input_size,
-    struct nnp_padding input_padding, struct nnp_size kernel_size,
-    struct nnp_size output_subsampling, const float* input, const float* kernel,
-    const float* bias, float* output, void* workspace_buffer,
-    size_t* workspace_size, enum nnp_activation activation,
-    const void* activation_parameters, pthreadpool_t threadpool,
-    struct nnp_profile* profile) {
+    enum nnp_convolution_transform_strategy transform_strategy, size_t input_channels,
+    size_t output_channels, struct nnp_size input_size, struct nnp_padding input_padding,
+    struct nnp_size kernel_size, struct nnp_size output_subsampling, const float *input,
+    const float *kernel, const float *bias, float *output, void *workspace_buffer,
+    size_t *workspace_size, enum nnp_activation activation, const void *activation_parameters,
+    pthreadpool_t threadpool, struct nnp_profile *profile) {
   NNP_TOTAL_START(profile)
 
   /* Basic validation of parameters. This check detects invalid, but not
    * unsupported parameters. */
   enum nnp_status status = validate_convolution_arguments(
-      1, input_channels, output_channels, input_size, input_padding,
-      kernel_size, output_subsampling, activation, activation_parameters);
+      1, input_channels, output_channels, input_size, input_padding, kernel_size,
+      output_subsampling, activation, activation_parameters);
   if (status != nnp_status_success) {
     goto cleanup;
   }
@@ -44,22 +42,20 @@ enum nnp_status nnp_convolution_depthwise_inference(
   }
 
   const struct nnp_size output_size = {
-      .width = (input_padding.left + input_size.width + input_padding.right -
-                kernel_size.width) /
+      .width = (input_padding.left + input_size.width + input_padding.right - kernel_size.width) /
                    output_subsampling.width +
                1,
-      .height = (input_padding.top + input_size.height + input_padding.bottom -
-                 kernel_size.height) /
-                    output_subsampling.height +
-                1};
+      .height =
+          (input_padding.top + input_size.height + input_padding.bottom - kernel_size.height) /
+              output_subsampling.height +
+          1};
 
-  size_t buffer_size = 1024;  // Buffer for a batch of output pixels
+  size_t buffer_size = 1024; // Buffer for a batch of output pixels
   for (size_t out_y = 0; out_y < output_size.height; out_y++) {
     for (size_t out_x = 0; out_x < output_size.width; out_x++) {
-      per_output_pixel_inference(out_x, out_y, input_channels, output_channels,
-                                 input_size, input_padding, kernel_size,
-                                 output_subsampling, input, kernel, output,
-                                 workspace_buffer, workspace_size);
+      per_output_pixel_inference(out_x, out_y, input_channels, output_channels, input_size,
+                                 input_padding, kernel_size, output_subsampling, input, kernel,
+                                 output, workspace_buffer, workspace_size);
     }
   }
 
@@ -68,9 +64,15 @@ cleanup:
   return status;
 }
 
-enum void per_output_pixel_inference(
-    size_t out_x, size_t out_y, size_t input_channels, size_t output_channels,
-    struct nnp_size input_size, struct nnp_padding input_padding,
-    struct nnp_size kernel_size, struct nnp_size output_subsampling,
-    const float* input, const float* kernel, float* output,
-    void* workspace_buffer, size_t* workspace_size) {}
+enum void per_output_pixel_inference(size_t out_x, size_t out_y, size_t input_channels,
+                                     size_t output_channels, struct nnp_size input_size,
+                                     struct nnp_padding input_padding, struct nnp_size kernel_size,
+                                     struct nnp_size output_subsampling, const float *input,
+                                     const float *kernel, float *output, void *workspace_buffer,
+                                     size_t *workspace_size) {
+  struct nnp_size input_offset = {.width = };
+  for (size_t filter_y = 0; filter_y < kernel_size.height; filter_y++) {
+    for (size_t filter_x = 0; filter_x < kernel_size.width, filter_x++) {
+    }
+  }
+}
