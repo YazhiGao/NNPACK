a9c9f057074fac608ea2193a70478810602d4963
Yazhi Gao <leon.gao19@gmail.com>
2018-04-23 00:17:04 -0400

    copy paster sequential solution for likun's stupid bench

--- src/convolution-depthwise-inference.c
+++ src/convolution-depthwise-inference.c
@@ -80,6 +80,65 @@ static void per_output_pixel_inference(size_t out_x, size_t out_y, size_t input_
     }
   }
 }
+#else
+struct convolution_depthwise_output_context {
+  size_t input_channels;
+  size_t output_channels;
+  size_t depthwise_multiplier;
+  struct nnp_size input_size;
+  struct nnp_padding input_padding;
+  struct nnp_size kernel_size;
+  struct nnp_size output_size;
+  struct nnp_size output_subsampling;
+  const float *input_pointer;
+  const float *kernel_pointer;
+  const float *bias;
+  float *output_pointer;
+};
+static void compute_convolution_depthwise_output(
+    const struct convolution_depthwise_output_context context[restrict static 1],
+    size_t output_channel) {
+  const size_t input_channels = context->input_channels;
+  const size_t output_channels = context->output_channels;
+  const size_t depthwise_multiplier = context->depthwise_multiplier;
+  const struct nnp_size input_size = context->input_size;
+  const struct nnp_padding input_padding = context->input_padding;
+  const struct nnp_size kernel_size = context->kernel_size;
+  const struct nnp_size output_size = context->output_size;
+  const struct nnp_size output_subsampling = context->output_subsampling;
+
+  // input layout NHWC
+  // kernel layout HWCX
+  // output layout NHW(CX)
+  const float(*input)[input_size.height][input_size.width][input_channels] =
+      (const float(*)[input_size.height][input_size.width][input_channels])context->input_pointer;
+  const float(*kernel)[kernel_size.width][input_channels][depthwise_multiplier] =
+      (const float(*)[kernel_size.width][input_channels][depthwise_multiplier])
+          context->kernel_pointer;
+  float(*output)[output_size.height][output_size.width][output_channels] =
+      (float(*)[output_size.height][output_size.width][output_channels])context->output_pointer;
+
+  size_t depthwise_channel = output_channel % depthwise_multiplier;
+  size_t input_channel = output_channel / depthwise_multiplier;
+
+  for (size_t y = 0; y < output_size.height; y++) {
+    for (size_t x = 0; x < output_size.width; x++) {
+      double v = 0.0;
+      for (size_t i = 0; i < kernel_size.height; i++) {
+        const size_t s = y * output_subsampling.height + i - input_padding.top;
+        if (s < input_size.height) {
+          for (size_t j = 0; j < kernel_size.width; j++) {
+            const size_t t = x * output_subsampling.width + j - input_padding.left;
+            if (t < input_size.width) {
+              v += input[0][s][t][input_channel] * kernel[i][j][input_channel][depthwise_channel];
+            }
+          }
+        }
+      }
+      output[0][y][x][output_channel] = v + context->bias[output_channel];
+    }
+  }
+}
 #endif
 
 enum nnp_status nnp_convolution_depthwise_inference(
@@ -124,9 +183,28 @@ enum nnp_status nnp_convolution_depthwise_inference(
     }
   }
 #else
-  nnp_convolution_depthwise_output__reference(
-      1, input_channels, output_channels, output_channels / input_channels, input_size,
-      input_padding, kernel_size, output_subsampling, input, kernel, bias, output, threadpool);
+  size_t depthwise_multiplier = output_channels / input_channels;
+  struct convolution_depthwise_output_context convolution_depthwise_output_context = {
+      .input_channels = input_channels,
+      .output_channels = output_channels,
+      .depthwise_multiplier = depthwise_multiplier,
+      .input_size = input_size,
+      .input_padding = input_padding,
+      .kernel_size = kernel_size,
+      .output_size = output_size,
+      .output_subsampling = output_subsampling,
+      .input_pointer = input,
+      .kernel_pointer = kernel,
+      .bias = bias,
+      .output_pointer = output};
+
+  pthreadpool_compute_1d(threadpool,
+                         (pthreadpool_function_1d_t)compute_convolution_depthwise_output,
+                         &convolution_depthwise_output_context, output_channels);
+  /** nnp_convolution_depthwise_output__reference( */
+  /**     1, input_channels, output_channels, output_channels / input_channels, input_size, */
+  /**     input_padding, kernel_size, output_subsampling, input, kernel, bias, output, threadpool);
+   */
 #endif
 cleanup:
   NNP_TOTAL_END(profile)
