ee1afd4093ee7053b610657d3e2ba0a0145df47c
Likun Lei <floatingcloud1994@gmail.com>
2018-05-05 19:23:06 -0400

    to be formart

--- src/convolution-depthwise-inference.c
+++ src/convolution-depthwise-inference.c
@@ -17,48 +17,30 @@
 // very naive implementation
 #include <nnpack/reference.h>
 
-struct convolution_depthwise_output_context {
-  size_t input_channels;
-  size_t output_channels;
-  size_t depthwise_multiplier;
-  struct nnp_size input_size;
-  struct nnp_padding input_padding;
-  struct nnp_size kernel_size;
-  struct nnp_size output_size;
-  struct nnp_size output_subsampling;
-  const float* input_pointer;
-  const float* kernel_pointer;
-  const float* bias;
-  float* output_pointer;
-};
 
-static void compute_convolution_depthwise_output(
-    const struct convolution_depthwise_output_context
-        context[restrict static 1],
-    size_t sample, size_t output_channel) {
-  const size_t input_channels = context->input_channels;
-  const size_t output_channels = context->output_channels;
-  const size_t depthwise_multiplier = context->depthwise_multiplier;
-  const struct nnp_size input_size = context->input_size;
-  const struct nnp_padding input_padding = context->input_padding;
-  const struct nnp_size kernel_size = context->kernel_size;
-  const struct nnp_size output_size = context->output_size;
-  const struct nnp_size output_subsampling = context->output_subsampling;
 
-  const float(*input)[input_channels][input_size.height][input_size.width] =
-      (const float(*)[input_channels][input_size.height][input_size.width])
-          context->input_pointer;
-  const float(
-      *kernel)[depthwise_multiplier][kernel_size.height][kernel_size.width] =
-      (const float(*)[depthwise_multiplier][kernel_size.height]
-                     [kernel_size.width])context->kernel_pointer;
-  float(*output)[output_channels][output_size.height][output_size.width] =
-      (float(*)[output_channels][output_size.height][output_size.width])
-          context->output_pointer;
 
+void nnp_convolution_depthwise_output_reference(
+    size_t batch_size, size_t input_channels, size_t output_channels,
+    size_t depthwise_multiplier, struct nnp_size input_size,
+    struct nnp_padding input_padding, struct nnp_size kernel_size,
+    struct nnp_size output_subsampling, const float input_pointer[],
+    const float kernel_pointer[], const float bias[], float output_pointer[],
+    pthreadpool_t threadpool) {
+  const struct nnp_size output_size = {
+      .width = (input_padding.left + input_size.width + input_padding.right -
+                kernel_size.width) /
+                   output_subsampling.width +
+               1,
+      .height = (input_padding.top + input_size.height + input_padding.bottom -
+                 kernel_size.height) /
+                    output_subsampling.height +
+                1};
   size_t depthwise_channel = output_channel % depthwise_multiplier;
   size_t input_channel = output_channel / depthwise_multiplier;
-
+  for (size_t b=0;b<batch_size;b++){
+    for (size_t oc=0;oc<output_channels;oc++){
+      
   for (size_t y = 0; y < output_size.height; y++) {
     for (size_t x = 0; x < output_size.width; x++) {
       double v = 0.0;
@@ -78,43 +60,8 @@ static void compute_convolution_depthwise_output(
       output[sample][output_channel][y][x] = v + context->bias[output_channel];
     }
   }
-}
-
-void nnp_convolution_depthwise_output_reference(
-    size_t batch_size, size_t input_channels, size_t output_channels,
-    size_t depthwise_multiplier, struct nnp_size input_size,
-    struct nnp_padding input_padding, struct nnp_size kernel_size,
-    struct nnp_size output_subsampling, const float input_pointer[],
-    const float kernel_pointer[], const float bias[], float output_pointer[],
-    pthreadpool_t threadpool) {
-  const struct nnp_size output_size = {
-      .width = (input_padding.left + input_size.width + input_padding.right -
-                kernel_size.width) /
-                   output_subsampling.width +
-               1,
-      .height = (input_padding.top + input_size.height + input_padding.bottom -
-                 kernel_size.height) /
-                    output_subsampling.height +
-                1};
-  struct convolution_depthwise_output_context
-      convolution_depthwise_output_context = {
-          .input_channels = input_channels,
-          .output_channels = output_channels,
-          .depthwise_multiplier = depthwise_multiplier,
-          .input_size = input_size,
-          .input_padding = input_padding,
-          .kernel_size = kernel_size,
-          .output_size = output_size,
-          .output_subsampling = output_subsampling,
-          .input_pointer = input_pointer,
-          .kernel_pointer = kernel_pointer,
-          .bias = bias,
-          .output_pointer = output_pointer};
-
-  pthreadpool_compute_2d(
-      threadpool,
-      (pthreadpool_function_2d_t)compute_convolution_depthwise_output,
-      &convolution_depthwise_output_context, batch_size, output_channels);
+    }
+  }
 }
 
 enum nnp_status nnp_convolution_depthwise_inference(
